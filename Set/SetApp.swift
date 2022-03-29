99d6e36c55ac6ea658361d4a672e5add2dbf431f
yesokcool <ysokcl@gmail.com>
2022-03-29 13:46:44 -0400

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Matteo Galli on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
