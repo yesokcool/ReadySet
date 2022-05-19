f279509ebda7c852d197d328e03343ccb663b709
yesokcool <ysokcl@gmail.com>
2022-05-19 13:00:07 -0400

    Add theme

--- Set/Theme.swift
+++ Set/Theme.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct Theme {
     let themeName: String
-    let numberOfPairsOfCardsToShow: Int
     let themeColor: String
     
-    init(themeName: String themeColor: String) {
+    init(themeName: String, themeColor: String) {
         self.themeName = themeName
-        self.numberOfPairsOfCardsToShow = contentSet.count
         self.themeColor = themeColor
     }
 }
